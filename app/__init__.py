3ac56fbb19d8ad6491716b3629f38f008bb604b1
Ricard Sampedro <ricard@onna.com>
2019-07-09 14:37:19 +0200

    Added more logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import logging, sys, os
 
 app = Flask(__name__)
 app.config.from_object(f'app.config.{app.env}')
 app.debug = app.config.get("DEBUG")
+
+if 'DYNO' in os.environ:
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
 
 from app.slackbot import blueprint as slack_bot
 
