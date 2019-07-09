b683ffebe3bb0da78fbeb67a6c3f4b2a27202fcc
Ricard Sampedro <ricard@onna.com>
2019-07-09 14:30:08 +0200

    Added more logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(f'app.config.{app.env}')
+app.debug = app.config.get("DEBUG")
 
 from app.slackbot import blueprint as slack_bot
 
